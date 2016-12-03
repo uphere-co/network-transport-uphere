d749efec659f52dcae1e5e2a91c44cafa84e9068
Ian-Woo Kim <ianwookim@gmail.com>
2016-12-03 08:23:21 +0000

    add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, base, bytestring, containers, data-accessor
+, network, network-transport, network-transport-tests, stdenv
+}:
+mkDerivation {
+  pname = "network-transport-uphere";
+  version = "0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base bytestring containers data-accessor network network-transport
+  ];
+  testHaskellDepends = [
+    base network network-transport network-transport-tests
+  ];
+  homepage = "http://haskell-distributed.github.com";
+  description = "UpHere specific network transport";
+  license = stdenv.lib.licenses.bsd3;
+}
